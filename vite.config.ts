c8327a0985f32cac58786fdddc4db0cbe1b95c80
aurelijusstanaitis <ditnet@gmail.com>
2025-01-15 13:01:20 +0200

    Refactor PWA configuration: simplify favicon links and update manifest settings

--- vite.config.ts
+++ vite.config.ts
@@ -8,14 +8,31 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon/favicon.ico', 'favicon/apple-touch-icon.png', 'favicon/favicon.svg', 'images/*'],
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon/favicon.ico",
+        "favicon/apple-touch-icon.png",
+        "favicon/favicon.svg",
+        "images/*",
+      ],
       manifest: {
-        name: "Duomenų konferencija - Hekatoncheiras",
-        short_name: "Duomenų konferencija",
-        description: "Jau tradicija tapusioje konferencijoje kviečiame susipažinti su Valstybės duomenų ežero ir moderniausių analitinių įrankių galimybėmis, leidžiančiomis efektyviai naudoti įvairius valstybės duomenis.",
+        id: "/",
+        name: "Hekatoncheiras 2025",
+        short_name: "Konferencija",
+        description:
+          "Jau tradicija tapusioje konferencijoje kviečiame susipažinti su Valstybės duomenų ežero ir moderniausių analitinių įrankių galimybėmis, leidžiančiomis efektyviai naudoti įvairius valstybės duomenis.",
+        start_url: "/",
+        scope: "/",
+        display: "standalone",
+        orientation: "portrait",
+        background_color: "#1c2228",
         theme_color: "#1c2228",
         icons: [
+          {
+            src: "/icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
           {
             src: "/favicon/web-app-manifest-192x192.png",
             sizes: "192x192",
